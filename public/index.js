9d54a45145adf1e5c7317d38cf7a33adef8f8890
kaspermoelans <104373919+kaspermoelans@users.noreply.github.com>
2024-04-08 16:19:11 +0200

    Update index.js

--- public/index.js
+++ public/index.js
@@ -11,7 +11,7 @@ canvasEl.width = window.innerWidth;
 canvasEl.height = window.innerHeight;
 const canvas = canvasEl.getContext("2d");
 
-const socket = io(`ws://localhost:1000`);
+const socket = io();
 
 let groundMap = [[]];
 let decalMap = [[]];
